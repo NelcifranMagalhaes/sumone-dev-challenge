ffb0a2450fda889e9de92fdf419eebb68151c51e
nelcifran <nelcifranpires@gmail.com>
2019-04-06 11:25:44 -0300

    rollback for 'form_tag'

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,8 +1,7 @@
 class DrinksController < ApplicationController
 
   def index
-    @q = Drink.order(name: :asc).ransack(params[:q])
-    @drinks = @q.result.page(params[:page]).per(10)
+  	@drinks = Drink.all.page(params[:page]).per(10)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'drinks#index'
-  post '/advanced_search' , to: 'drinks#advanced_search'
-  get '/advanced_search' , to: 'drinks#index'
-
+  post '/drinks' , to: 'drinks#index'
   resources :home, only: %i[index]
 end
