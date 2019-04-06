289f30668c12587bc0c76318746cf8c26c7a480f
nelcifran <nelcifranpires@gmail.com>
2019-04-06 13:09:36 -0300

    initial search for advanced search and simple search

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,7 +1,26 @@
 class DrinksController < ApplicationController
 
   def index
-  	@drinks = Drink.all.page(params[:page]).per(10)
+  	@drinks = Drink.page(params[:page]).per(10)
+  	name = params['name']
+  	temperature = params['temperature']
+  	alcohol_level = params['alcohol_level']
+  	sugar = params['sugar']
+  	search = params['search']
+
+  	if search == 'simple'
+	  	if name.present?
+			@drinks = Drink.where("name like '%#{name}%'").page(params[:page]).per(10)
+	  	end
+
+	 elsif search == 'advanced'
+	 	if temperature.present? || alcohol_level.present? || sugar.present?
+	 		@drinks = @drinks.where("temperature = #{temperature}") unless temperature.blank?
+	 		# @drinks = @drinks.where("alcohol_level ")
+	 		@drinks.page(params[:page]).per(10)
+	 	end
+
+  	end
   end
 
 end
